0e8a0d10ebdaed1280dc3ce62f4f671cc12a8787
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-01-10 14:17:37 +0900

    [Silver III] Title: 삼각형 만들기, Time: 668 ms, Memory: 75996 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.py
@@ -0,0 +1,16 @@
+import sys
+
+# 입력
+N = int(sys.stdin.readline())
+A = [int(sys.stdin.readline()) for _ in range(N)]
+
+# 정렬
+A.sort()
+
+answer = -1
+for i in range(N - 1, 1, -1):
+    if A[i] < A[i-1] + A[i-2]:
+        answer = A[i] + A[i-1] + A[i-2]
+        break
+
+print(answer)
