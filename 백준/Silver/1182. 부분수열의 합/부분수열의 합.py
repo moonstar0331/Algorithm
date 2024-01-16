3e926ad68622b4cfa8a41008d5beefb2c3934d67
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-01-16 16:56:23 +0900

    [Silver II] Title: 부분수열의 합, Time: 256 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,13 +1,21 @@
-from itertools import combinations
+import sys
 
 # 입력
-N, S = map(int, input().split())
-A = list(map(int, input().split()))
-
+n, S = map(int, sys.stdin.readline().split(' '))
+nums = list(map(int, sys.stdin.readline().split(' ')))
 ans = 0
-for num_elements in range(1, N+1):
-    for c in list(combinations(A, num_elements)):
-        if sum(c) == S:
-            ans += 1
 
+# 재귀함수
+def rec_func(k, value):
+    if k == n:
+        global ans
+        if value == S:
+            ans += 1
+    else:
+        rec_func(k+1, value + nums[k])
+        rec_func(k+1, value)
+
+rec_func(0, 0)
+if S == 0:
+    ans -= 1
 print(ans)
