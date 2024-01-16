de77264faa5e913088523a8ca5966ec8a5388eea
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-01-16 11:34:20 +0900

    [Silver III] Title: N과 M (3), Time: 1260 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,20 @@
+import sys
+
+# 입력
+N, M = map(int, sys.stdin.readline().split(' '))
+
+selected = [0 for _ in range(M)]
+used = [0 for _ in range(N+1)]
+
+def rec_func(k):
+    if k == M:
+        for x in selected:
+            sys.stdout.write(str(x) + ' ')
+        sys.stdout.write('\n')
+    else:
+        for cand in range(1, N+1):
+            selected[k] = cand
+            rec_func(k+1)
+            selected[k] = 0
+
+rec_func(0)
