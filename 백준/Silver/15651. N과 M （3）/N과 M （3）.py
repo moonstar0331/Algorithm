3d4830affd69dd3c357a9d078138a55919015fde
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-23 02:13:06 +0900

    [Silver III] Title: N과 M (3), Time: 1168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,20 +1,18 @@
 import sys
+n, m = map(int, sys.stdin.readline().split(' '))
 
-# 입력
-N, M = map(int, sys.stdin.readline().split(' '))
-
-selected = [0 for _ in range(M)]
-used = [0 for _ in range(N+1)]
+selected = [0 for _ in range(m)]
+used = [0 for _ in range(n + 1)]
 
 def rec_func(k):
-    if k == M:
+    if k == m:
         for x in selected:
             sys.stdout.write(str(x) + ' ')
         sys.stdout.write('\n')
     else:
-        for cand in range(1, N+1):
+        for cand in range(1, n+1):
             selected[k] = cand
-            rec_func(k+1)
+            rec_func(k + 1)
             selected[k] = 0
-
+            
 rec_func(0)
