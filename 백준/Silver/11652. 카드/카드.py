2049ed0805a199eccd55cd14cd44d325d8d5ce14
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-23 04:01:35 +0900

    [Silver IV] Title: 카드, Time: 120 ms, Memory: 36372 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -1,18 +1,15 @@
 import sys
-
-# 입력
 n = int(sys.stdin.readline())
 a = []
 
 for i in range(n):
     a.append(int(sys.stdin.readline()))
 
-# 정렬
 a.sort()
 
-mode = a[0] # 최빈값
-modeCnt = 1 # 최빈값 개수
-curCnt = 1 # 현재 보고 있는 수의 개수
+mode = a[0]
+modeCnt = 1
+curCnt = 1
 
 for i in range(1, n):
     if a[i] == a[i-1]:
@@ -22,5 +19,5 @@ for i in range(1, n):
     if modeCnt < curCnt:
         modeCnt = curCnt
         mode = a[i]
-
+        
 print(mode)
