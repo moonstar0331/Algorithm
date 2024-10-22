2ede58e82ea4fda2fcf2d0acab614ab4ff75f21e
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-23 03:52:20 +0900

    [Silver IV] Title: 국영수, Time: 436 ms, Memory: 65212 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -6,7 +6,7 @@ class Elem:
         self.korean = korean
         self.english = english
         self.math = math
-
+        
 n = int(sys.stdin.readline())
 a = []
 
@@ -14,7 +14,7 @@ for i in range(n):
     inp = sys.stdin.readline().split()
     inp[1:] = map(int, inp[1:])
     a.append(Elem(*inp))
-
+    
 a.sort(key = lambda x: (-x.korean, x.english, -x.math, x.name))
 
 names = [elem.name for elem in a]
