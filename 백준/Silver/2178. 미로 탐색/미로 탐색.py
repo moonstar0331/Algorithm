0bd4a3314db1c265a20da708e55aa01ba1eea746
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-24 04:12:23 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,9 +1,8 @@
 import sys
 from collections import deque
-
-input = sys.stdin.readline
-n, m = map(int, input().split())
-a = [input().strip() for _ in range(n)]
+si = sys.stdin.readline
+n, m = list(map(int, si().split()))
+a = [si().strip() for _ in range(n)]
 
 dist = [[0] * m for _ in range(n)]
 dir = [[1, 0], [0, 1], [-1, 0], [0, -1]]
@@ -13,7 +12,7 @@ def bfs():
     queue.append(0)
     queue.append(0)
     dist[0][0] = 1
-
+    
     while queue:
         x = queue.popleft()
         y = queue.popleft()
@@ -25,9 +24,9 @@ def bfs():
                 continue
             if a[nx][ny] == '0':
                 continue
-            dist[nx][ny] =  dist[x][y] + 1
+            dist[nx][ny] = dist[x][y] + 1
             queue.append(nx)
             queue.append(ny)
-
+            
 bfs()
-print(dist[n-1][m-1])
+print(dist[n - 1][m - 1])
