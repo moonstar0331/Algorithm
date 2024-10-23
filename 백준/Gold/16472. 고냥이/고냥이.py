fa9c841ed26d388fee48fa6f7b3a352eefad406e
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-24 02:49:10 +0900

    [Gold IV] Title: 고냥이, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.py
+++ 백준/Gold/16472. 고냥이/고냥이.py
@@ -1,9 +1,7 @@
 import sys
-input = sys.stdin.readline
-
-# 입력
-n = int(input())
-a = input().strip()
+si = sys.stdin.readline
+n = int(si())
+a = si().strip()
 cnt = [0] * 30
 kind = 0
 
@@ -13,14 +11,14 @@ def add(x):
     cnt[t] += 1
     if cnt[t] == 1:
         kind += 1
-    
+        
 def erase(x):
     global kind
     t = ord(x) - ord('a')
     cnt[t] -= 1
     if cnt[t] == 0:
         kind -= 1
-
+        
 L = 0
 ans = 0
 for R in range(len(a)):
@@ -28,6 +26,6 @@ for R in range(len(a)):
     while kind > n:
         erase(a[L])
         L += 1
-    ans = max(ans, R-L+1)
-
+    ans = max(ans, R - L + 1)
+    
 print(ans)
