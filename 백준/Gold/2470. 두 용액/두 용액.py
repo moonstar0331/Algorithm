0d8dd6a3f80aba2689993c7bb85d0cd311e867b5
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-24 02:36:41 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -3,27 +3,13 @@ si = sys.stdin.readline
 n = int(si())
 a = sorted(list(map(int, si().split())))
 
-def lower_bound(a, l, r, x):
-    res = r + 1
-    while l <= r:
-        mid = (l + r) // 2
-        if a[mid] >= x:
-            res = mid
-            r = mid - 1
-        else: l = mid + 1
-    return res
-
-best_sum = 1 << 31
-v1, v2 = 0, 0
-
-for l in range(n - 1):
-    cand = lower_bound(a, l + 1, n - 1, -a[l])
-    if l < cand - 1 and abs(a[l] + a[cand-1]) < best_sum:
-        best_sum = abs(a[l] + a[cand -1])
-        v1, v2 = a[l], a[cand-1]
-        
-    if cand < n and abs(a[l] + a[cand]) < best_sum:
-        best_sum = abs(a[l] + a[cand])
-        v1, v2 = a[l],  a[cand]
+L, R = 0, n - 1
+v1, v2, best_sum = 0, 0, 1 << 31
+while L < R:
+    if best_sum > abs(a[L]+ a[R]):
+        best_sum = abs(a[L] + a[R])
+        v1, v2 = a[L], a[R]
+    if a[L] + a[R] > 0: R -= 1
+    else: L += 1
         
 print(v1, v2)
