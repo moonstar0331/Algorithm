6e41e286c49db05d3e4d108fd9e795ce12c6051d
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-24 02:32:39 +0900

    [Gold IV] Title: 부분합, Time: 116 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,22 +1,17 @@
 import sys
-input = sys.stdin.readline
-
-# 입력
-n, S = map(int, input().split())
-a = list(map(int, input().split()))
-
-R, sum, ans = -1, 0, n+1
-
+si = sys.stdin.readline
+n, S = list(map(int, si().split()))
+a = list(map(int, si().split()))
+R, sum, ans = -1, 0, n + 1
 for L in range(n):
-    while R+1 < n and sum < S:
+    while R + 1 < n and sum < S:
         R += 1
         sum += a[R]
-
+        
     if sum >= S:
-        ans = min(ans, R-L+1)
-    
+        ans = min(ans, R - L + 1)
+        
     sum -= a[L]
-
-if ans == n+1:
-    ans = 0
+    
+if ans == n + 1: ans = 0
 print(ans)
