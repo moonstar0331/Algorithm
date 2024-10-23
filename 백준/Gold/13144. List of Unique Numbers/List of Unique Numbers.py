b01c1e9f3971f45481d6a6456d7ab17414c9074b
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-24 02:41:03 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 116 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -1,18 +1,16 @@
 import sys
-input = sys.stdin.readline
-
-# 입력
-n = int(input())
-a = list(map(int, input().split()))
+si = sys.stdin.readline
+n = int(si())
+a = list(map(int, si().split()))
 cnt = [0] * 100005
 
 ans = 0
 R = -1
 for L in range(n):
-    while R+1 < n and cnt[a[R+1]] == 0:
+    while R + 1 < n and cnt[a[R + 1]] == 0:
         R += 1
         cnt[a[R]] += 1
     ans += R - L + 1
     cnt[a[L]] -= 1
-
+    
 print(ans)
