2011b6a3399d13874506eeb73a7bfbc13e5b1928
MoonSung Kim / 김문성 <moonsung0331@gmail.com>
2024-10-24 01:53:22 +0900

    [Gold V] Title: 두 용액, Time: 256 ms, Memory: 41932 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -15,14 +15,15 @@ def lower_bound(a, l, r, x):
 
 best_sum = 1 << 31
 v1, v2 = 0, 0
-for l in range(n - 1):
-    candidate = lower_bound(a, l + 1, n - 1, -a[l])
-    if l < candidate - 1 and abs(a[l] + a[candidate - 1]) < best_sum:
-        best_sum = abs(a[l] + a[candidate - 1])
-        v1, v2 = a[l], a[candidate - 1]
-    
-    if candidate < n and abs(a[l] + a[candidate]) < best_sum:
-        best_sum = abs(a[l] + a[candidate])
-        v1, v2 = a[l], a[candidate]
 
+for l in range(n - 1):
+    cand = lower_bound(a, l + 1, n - 1, -a[l])
+    if l < cand - 1 and abs(a[l] + a[cand-1]) < best_sum:
+        best_sum = abs(a[l] + a[cand -1])
+        v1, v2 = a[l], a[cand-1]
+        
+    if cand < n and abs(a[l] + a[cand]) < best_sum:
+        best_sum = abs(a[l] + a[cand])
+        v1, v2 = a[l],  a[cand]
+        
 print(v1, v2)
